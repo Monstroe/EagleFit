e5de222cc28770b265c6dcf2328f4d1827824532
Monstroe <colemontrose@gmail.com>
2022-10-15 18:03:56 -0400

    Started SQL Stuff

---
+++ app/src/main/java/com/example/eaglefit/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package com.example.eaglefit;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "EagleFitData";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String[] tables = {
+
+    };
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+}
