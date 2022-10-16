0dcd5b6b6fd0cf251665ba6e0b63384acbf9028f
Monstroe <colemontrose@gmail.com>
2022-10-16 00:42:36 -0400

    Update DatabaseHelper.java

--- app/src/main/java/com/example/eaglefit/DatabaseHelper.java
+++ app/src/main/java/com/example/eaglefit/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.eaglefit;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,6 +9,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "EagleFitData";
     private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_WORKOUTS = "Workouts";
+    private static final String TABLE_USER_PROGRESS = "UserProgress";
+    private static final String TABLE_USER_INFO = "UserInformation";
 
     private static final String[] tables = {
 
@@ -18,12 +23,28 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String workouts = "CREATE TABLE " + TABLE_WORKOUTS + "(EXERCISE_NAME TEXT, CHEST INTEGER, BACK INTEGER, SHOULDERS INTEGER, ARMS INTEGER, LEGS INTEGER)";
+        String userProgress = "CREATE TABLE " + TABLE_USER_PROGRESS + "(WORKOUT_COUNTER INTEGER PRIMARY KEY AUTOINCREMENT, DATE INTEGER, BENCH_PRESS REAL, SQUATS REAL, DEADLIFTS REAL)"; //ADD ALL EXERCISES HERE
+        String userInfo = "CREATE TABLE " + TABLE_USER_INFO + "(NAME TEXT, BIRTHDAY INTEGER, HEIGHT REAL, WEIGHT REAL)";
 
+        db.execSQL(workouts);
+        db.execSQL(userProgress);
+        db.execSQL(userInfo);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_WORKOUTS);
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_USER_PROGRESS);
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_USER_INFO);
+    }
 
+    public boolean addData(String tableName, ) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        //if(table.equals(TABLE))
+        //contentValues.put();
     }
 }
