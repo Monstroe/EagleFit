b4a49186413dbd68c9ad08db3e6e6bff897ab746
Monstroe <colemontrose@gmail.com>
2022-11-01 19:10:09 -0400

    More work done

--- app/src/main/java/com/example/eaglefit/MuscleSearchActivity.java
+++ app/src/main/java/com/example/eaglefit/MuscleSearchActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -14,6 +16,8 @@ public class MuscleSearchActivity extends AppCompatActivity {
 
     private static final String TAG = "MuscleSearchFragment";
 
+    private WorkoutData[] exercises;
+
     private ListView lvMuscleSearch;
 
     @Override
@@ -22,15 +26,39 @@ public class MuscleSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_muscle_search);
 
         lvMuscleSearch = (ListView) findViewById(R.id.muscle_search_listView);
-
         populateListView();
+        addEventToListView();
+
+    }
+
+    private void addEventToListView() {
+        lvMuscleSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String selectedItemName = (String) adapterView.getItemAtPosition(position);
+                for(WorkoutData exercise : exercises) {
+                    if(exercise.getExerciseName().equals(selectedItemName)) {
+                        Bundle bundle = new Bundle();
+                        bundle.putString("ExerciseName", exercise.getExerciseName());
+                        bundle.putString("ExerciseDesc", exercise.getExerciseDescription());
+                        //Navigate to fragment
+                    }
+                }
+            }
+        });
     }
 
     private void populateListView() {
         Intent intent = getIntent();
-        WorkoutData[] exercises = (WorkoutData[]) intent.getParcelableArrayExtra("Exercises");
+        exercises = (WorkoutData[]) intent.getParcelableArrayExtra("Exercises");
 
-        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exercises);
+        String[] exerciseNames = new String[exercises.length];
+        for(int i = 0; i < exerciseNames.length; i++) {
+            exerciseNames[i] = exercises[i].getExerciseName();
+        }
+
+        ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exerciseNames);
         lvMuscleSearch.setAdapter(adapter);
     }
+
 }
