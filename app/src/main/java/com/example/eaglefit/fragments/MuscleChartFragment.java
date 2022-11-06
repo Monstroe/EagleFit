061a46d714795d63794479975840ca64f0d58083
DonayT <DONAYT@my.erau.edu>
2022-11-06 11:33:16 -0500

    MuscleDudeAddition
    Added muscle guy, switch not working

--- app/src/main/java/com/example/eaglefit/fragments/MuscleChartFragment.java
+++ app/src/main/java/com/example/eaglefit/fragments/MuscleChartFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.example.eaglefit.R;
 
@@ -25,6 +26,9 @@ public class MuscleChartFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // New switch for chart flipper
+    Switch chartFlipper;
 
     public MuscleChartFragment() {
         // Required empty public constructor
@@ -51,10 +55,18 @@ public class MuscleChartFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        chartFlipper = (Switch) chartFlipper.findViewById(R.id.chartFlipper);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+//        if(chartFlipper.isChecked()){
+//
+//        }else{
+//
+//        }
     }
 
     @Override
@@ -63,4 +75,5 @@ public class MuscleChartFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_muscle_chart, container, false);
     }
+
 }
