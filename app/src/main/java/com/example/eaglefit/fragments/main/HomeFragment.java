d2afa777f7f42ec8aefc9d3d3d71b34ee0a102c9
DarthSimpius <113943517+DarthSimpius@users.noreply.github.com>
2022-12-02 14:05:24 -0500

    Added Graphs
    Added graphs to the homefragment java and xml files. No database functionality yet.

--- app/src/main/java/com/example/eaglefit/fragments/main/HomeFragment.java
+++ app/src/main/java/com/example/eaglefit/fragments/main/HomeFragment.java
@@ -22,6 +22,12 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.TimeZone;
+import java.util.Random;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class HomeFragment extends Fragment {
 
@@ -35,6 +41,9 @@ public class HomeFragment extends Fragment {
 
     private Button startWorkoutBtn;
 
+    private GraphView graphView;
+    private String graph_exercise = Random_Exercise();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +55,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        graphView = view.findViewById(R.id.idGraphView);
 
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
         int dayOfWeekInt = calendar.get(Calendar.DAY_OF_WEEK);
@@ -88,8 +98,58 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        try {
+            //Adds datapoints to the graph and sets it up
+            BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[]{
+                    new DataPoint(0, 1),
+                    new DataPoint(1, 3),
+                    new DataPoint(2, 4),
+                    new DataPoint(3, 9),
+                    new DataPoint(4, 6),
+                    new DataPoint(5, 3),
+                    new DataPoint(6, 6),
+                    new DataPoint(7, 1),
+                    new DataPoint(8, 2)
+            });
+            //BarGraphSeries<DataPoint> series[7] = Create_DataPoints(graph_exercise);
+            graphView.setTitle("" + graph_exercise + " Progress");
+            graphView.setTitleColor(R.color.purple_200);
+            graphView.setTitleTextSize(58);
+            graphView.addSeries(series);
+        }
+        catch (Exception e) {
+            graphView.setTitle("Error fetching data");
+        }
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    /* public BarGraphSeries<DataPoint> Create_DataPoints(String exercise) {
+        private BarGraphSeries<DataPoint> graph_data = new BarGraphSeries<DataPoint>(new DataPoint[7]);
+        saved_exercise_date = new exercise data
+        private int total_data = saveddata.gettotal???();
+
+        private int graph_counter = 0;
+        for (int i = 0; i < 7; i++) {
+            if (exercise.equals(exercise_data)) {
+                graph_data[graph_counter] = new DataPoint(graph_counter, data.sits*data.reps);
+                graph_counter++;
+            }
+        }
+
+
+    } */
+    public String Random_Exercise(){
+        int rand_exercise = new Random().nextInt(3) + 1;
+        String exercise = "";
+
+        if (rand_exercise == 1)
+            exercise = "Bench Press";
+        else if (rand_exercise == 2)
+            exercise = "Push-Ups";
+        else if (rand_exercise == 3)
+            exercise = "Sit-Ups";
+        return exercise;
     }
 }
